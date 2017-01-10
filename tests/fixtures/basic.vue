f3dbfb4131baef8192dfb88041837a33bd6314c1
Gabe Anguiano <gabe.anguiano@gmail.com>
2017-01-09 23:25:18 -0800

    basic test fixture matches hello world example for single file components on vuejs.org

--- tests/fixtures/basic.vue
+++ tests/fixtures/basic.vue
@@ -1,20 +1,19 @@
 <template>
-  <h2 class="red">{{msg}}</h2>
-  <style></style>
+  <p>{{ greeting }} World!</p>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      msg: 'Hello from Component A!'
+  module.exports = {
+    data: function() {
+      return {
+        greeting: "Hello"
+      };
     }
-  }
-}
-</script>
+  };
 
-<style>
-comp-a h2 {
-  color: #f00;
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
 }
 </style>
